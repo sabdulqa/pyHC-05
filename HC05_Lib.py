41432378014e46ab843a714a1f912b1eb8a46817
System Administrator <root@Insomnias-MacBook-Pro.local>
2017-08-21 02:14:27 -0500

    add HC05_Lib.py

---
+++ HC05_Lib.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+
+
+import serial as ser
+
+# initialize HC-05
+def init_HC05():
+    print "this is HC-05 library init"
+
+
+
+# connection
+def init_connect():
+    print "Start HC05 connection stablishment"
+
+# Read Data
+def read_data():
+    print "Read data from HC05"
+
+
+# Send Data
+def send_data():
+    print "send data to HC05 module"
